91ff7152cf2804073db96ea0579175a87169148b
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-17 10:48:17 +0300

    handle delay on register navigation

--- lib/features/register/presentation/widgets/first_view_body.dart
+++ lib/features/register/presentation/widgets/first_view_body.dart
@@ -3,38 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 class FirstViewBody extends StatelessWidget {
-  const FirstViewBody({super.key, required this.body});
+  const FirstViewBody({super.key, required this.body, this.animate = false});
   final Widget body;
+  final bool animate;
+  
   @override
   Widget build(BuildContext context) {
+    Widget content = Container(
+      clipBehavior: Clip.antiAlias,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadiusDirectional.only(
+          topEnd: Radius.circular(100),
+        ),
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      child: animate 
+          ? FadeIn(
+              delay: Duration(milliseconds: 1500),
+              duration: Duration(seconds: 1),
+              child: body,
+            )
+          : body,
+    );
+
+    if (animate) {
+      content = content
+          .animate()
+          .fade(delay: 500.ms, duration: 600.ms)
+          .slide(
+            begin: Offset(
+              Directionality.of(context) == TextDirection.rtl ? -0.8 : 0.8,
+              0.8,
+            ),
+            end: Offset.zero,
+            delay: 300.ms,
+            duration: 900.ms,
+          );
+    }
+
     return SliverToBoxAdapter(
-      child:
-          Container(
-                clipBehavior: Clip.antiAlias,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusDirectional.only(
-                    topEnd: Radius.circular(100),
-                  ),
-                  color: Theme.of(context).colorScheme.surface,
-                ),
-                child: FadeIn(
-                  delay: Duration(milliseconds: 1500),
-                  duration: Duration(seconds: 1),
-                  child: body,
-                ),
-              )
-              .animate()
-              .fade(delay: 500.ms, duration: 600.ms)
-              .slide(
-                begin: Offset(
-                  Directionality.of(context) == TextDirection.rtl ? -0.8 : 0.8,
-                  0.8,
-                ),
-                end: Offset.zero,
-                delay: 300.ms,
-                duration: 900.ms,
-              ),
+      child: content,
     );
   }
 }
