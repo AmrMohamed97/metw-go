ebc06b2aea5317b7d1afc839b40b98c5f76b7f88
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-19 07:14:38 +0300

    finish editing otp page design

--- lib/features/otp/presentation/page/otp_page.dart
+++ lib/features/otp/presentation/page/otp_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:metw_go/core/l10n/app_localizations.dart';
@@ -66,7 +67,10 @@ class OtpPage extends StatelessWidget {
               isMax: true,
             ),
             Spacer(flex: 4),
-          ],
+          ].animate(interval: 100.ms)
+           .fade(duration: 700.ms, curve: Curves.easeOut)
+           .slideY(begin: 0.2, end: 0, duration: 700.ms, curve: Curves.easeOutCubic)
+           .scale(begin: const Offset(0.95, 0.95), end: const Offset(1, 1), duration: 700.ms, curve: Curves.easeOutCubic),
         ),
       ),
     );
