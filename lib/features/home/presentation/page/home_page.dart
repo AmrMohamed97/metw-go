f7e1f77d8613eea7f8fe04a99d871e9d5e7623bf
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-23 04:26:55 +0300

    edit home orders

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:metw_go/core/di/dependency_injection.dart';
-import 'package:metw_go/core/theme/app_text_style.dart';
 import 'package:metw_go/core/utils/app_images.dart';
-import 'package:metw_go/core/widgets/order_item.dart';
 import 'package:metw_go/core/widgets/screen_wrapper.dart';
 import 'package:metw_go/features/home/presentation/manager/home_cubit.dart';
 import 'package:metw_go/features/home/presentation/manager/home_state.dart';
+import 'package:metw_go/features/home/presentation/services/home_orders_view.dart';
 import 'package:metw_go/features/home/presentation/view/home_today_earnings.dart';
 import 'package:metw_go/features/home/presentation/view/home_wallet_view.dart';
 import 'package:metw_go/features/home/presentation/widgets/home_page_app_bar.dart';
@@ -47,136 +46,14 @@ class HomePage extends StatelessWidget {
                   cubit.status == 'offline'
                       ? Image.asset(AppImages.offline)
                       // Upcoming Orders Header
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "طلبات قادمة",
-                              style: AppTextStyle.medium16(context).copyWith(
-                                color: Theme.of(context).colorScheme.tertiary,
-                              ),
-                            ),
-                            Text(
-                              "عرض الكل",
-                              style: AppTextStyle.medium14(context).copyWith(
-                                color: Theme.of(context).colorScheme.primary,
-                              ),
-                            ),
-                          ],
-                        ),
-                  16.verticalSpace,
+                      : HomeOrdersView(),
 
-                  // Active Order
-                  const OrderItem(
-                    orderId: "#MET-8842",
-                    distance: "4.2 كم",
-                    isUrgent: true,
-                    pickup: "مطعم شواية الخليج - طريق التخصصي",
-                    delivery: "حي النخيل - شارع الأمير سعود",
-                    isTodayOrders: true,
-                  ),
-                  16.verticalSpace,
-
-                  // Small Order Item 1
-                  _buildSmallOrderItem(
-                    context: context,
-                    orderId: "#MET-8845",
-                    showArrow: true,
-                  ),
-                  16.verticalSpace,
-
-                  // Small Order Item 2
-                  _buildSmallOrderItem(
-                    context: context,
-                    orderId: "#MET-8845",
-                    showSwipeAction: true,
-                  ),
                   40.verticalSpace,
                 ],
               ),
             );
           },
         ),
-      ),
-    );
-  }
-
-  Widget _buildSmallOrderItem({
-    required BuildContext context,
-    required String orderId,
-    bool showArrow = false,
-    bool showSwipeAction = false,
-  }) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.04),
-            blurRadius: 15,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(12.w),
-            child: Container(
-              padding: EdgeInsets.all(10.w),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.outline,
-                borderRadius: BorderRadius.circular(12.r),
-              ),
-              child: Icon(
-                Icons.inventory_2_outlined,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-            ),
-          ),
-          SizedBox(width: 4.w),
-          Text(
-            orderId,
-            style: AppTextStyle.medium16(
-              context,
-            ).copyWith(color: Theme.of(context).colorScheme.tertiary),
-          ),
-          const Spacer(),
-          if (showArrow)
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              child: Icon(
-                Icons.chevron_right_outlined,
-                color: Theme.of(context).colorScheme.onSurface,
-                size: 24.sp,
-              ),
-            ),
-          if (showSwipeAction)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                borderRadius: BorderRadiusDirectional.horizontal(
-                  start: Radius.circular(30),
-                  end: Radius.circular(30),
-                ),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    "طلب جاري $orderId",
-                    style: AppTextStyle.medium14(
-                      context,
-                    ).copyWith(color: Colors.white),
-                  ),
-                  SizedBox(width: 8.w),
-                  Icon(Icons.more_horiz, color: Colors.white, size: 20.sp),
-                ],
-              ),
-            ),
-        ],
       ),
     );
   }
