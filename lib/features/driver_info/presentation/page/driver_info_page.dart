d5bcad6c8769ea2cfab694343603141dc53a4085
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-29 08:36:52 +0300

    add driver info page part1

---
+++ lib/features/driver_info/presentation/page/driver_info_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DriverInfoPage extends StatelessWidget {
+  const DriverInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
