b51ad8b57208078759222aae5a4d948393153184
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-29 17:28:56 +0300

    edit

---
+++ lib/features/service_areas/presentation/page/service_areas_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:metw_go/core/widgets/screen_wrapper.dart';
+
+class ServiceAreasPage extends StatelessWidget {
+  const ServiceAreasPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenWrapper(
+      body: SizedBox(),
+    );
+  }
+}
