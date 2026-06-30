203d50c85e675d3f0f25e387528e61049fc0fcd3
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-30 14:14:04 +0300

    edit splash

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -4,11 +4,16 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:go_router/go_router.dart';
 import 'package:metw_go/core/cubit/app_cubit.dart';
 import 'package:metw_go/core/cubit/app_state.dart';
+import 'package:metw_go/core/router/app_routes.dart';
 import 'package:metw_go/core/utils/app_images.dart';
 import 'package:metw_go/core/widgets/custom_error_widget.dart';
 import 'package:metw_go/core/widgets/screen_wrapper.dart';
+import 'package:metw_go/features/home/presentation/services/location_service.dart';
+import 'package:metw_go/features/home/presentation/services/tracking_location_service.dart';
+import 'package:permission_handler/permission_handler.dart' as ph;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -49,11 +54,9 @@ class _SplashScreenState extends State<SplashScreen> {
                           )
                           .pulse(duration: 1800.ms, curve: Curves.easeInOut),
                       //--------------------------------------------------------------------------
-                       state is HomeStatusLoading
-                      ?SizedBox()
-                      :appCubit.status=='offline'
-                      ?//navigate to mainview
-                      : ,//open location start tracking then navigate to base view
+                      state is HomeStatusLoading
+                          ? SizedBox()
+                          : NextPage(), //open location start tracking then navigate to base view
                       const Spacer(),
                       SpinKitThreeBounce(
                         color: Theme.of(context).primaryColor,
@@ -66,5 +69,46 @@ class _SplashScreenState extends State<SplashScreen> {
         );
       },
     );
+  }
+}
+
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  void initState() {
+    if (BlocProvider.of<AppCubit>(context).status == 'offline') {
+      context.go(AppRoutes.mainView);
+    } else {
+      nextPage();
+    }
+    super.initState();
+  }
+
+  void nextPage() async {
+    final locationService = LocationService();
+    bool serviceEnabled = await locationService
+        .checkAndRequestLocationService();
+    if (!serviceEnabled) {
+      await ph.openAppSettings();
+      // return;
+    }
+    bool permissionGranted = await locationService.checkAndRequestPermission();
+    if (!permissionGranted) {
+      await ph.openAppSettings();
+      // return;
+    }
+    TrackingLocationService().startTracking(driverId: '9');
+    context.go(AppRoutes.mainView);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
   }
 }
