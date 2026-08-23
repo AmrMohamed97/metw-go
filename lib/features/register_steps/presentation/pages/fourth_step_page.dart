35460e6edd6f634d9331032f15df34d3106ab4e1
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-08-23 13:57:00 +0300

    fix: update navigation route on successful submission in FourthStepPage

--- lib/features/register_steps/presentation/pages/fourth_step_page.dart
+++ lib/features/register_steps/presentation/pages/fourth_step_page.dart
@@ -30,7 +30,7 @@ class FourthStepPage extends StatelessWidget {
             message: AppLocalizations.of(context)!.savedSuccessfully,
             state: ToastStates.success,
           );
-          context.go(AppRoutes.watin); // Proceeding to OTP or equivalent
+          context.go(AppRoutes.commingSoonPage); // Proceeding to OTP or equivalent
         } else if (state is SubmitFourthStepFailure) {
           showToast(context, message: state.message, state: ToastStates.error);
         }
