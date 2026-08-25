54909621f4681536119136125da7345f2bf38ac0
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-08-25 13:56:18 +0300

    feat: implement initial scaffold pages for confirm pickup and confirm dropoff features

---
+++ lib/features/confirm_dropoff/presentation/page/confirm_dropoff_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfirmDropoffPage extends StatelessWidget {
+  const ConfirmDropoffPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/confirm_pickup/presentation/page/confirm_pickup_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConfirmPickupPage extends StatelessWidget {
+  const ConfirmPickupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
